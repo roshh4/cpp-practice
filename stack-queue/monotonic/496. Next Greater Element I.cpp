2d0a2eba426fa6773ab77bdaf7585eddb907cf62
roshh4 <roshinidharmaraj@gmail.com>
2026-09-13 12:33:07 +0530

    next greater element 1

--- stack-queue/monotonic/496. Next Greater Element I.cpp
+++ stack-queue/monotonic/496. Next Greater Element I.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        map<int,int> pp;
-        stack<int> st;
-        for(int i = nums2.size() - 1; i >= 0; i--){
-            while(!st.empty() && st.top() <= nums2[i]){
-                st.pop();
-            }
-            if(!st.empty()) pp[nums2[i]] = st.top();
-            else pp[nums2[i]] = -1;
-            st.push(nums2[i]);
-        }
+        stack<int> mono;
+        map<int,int> mpp;
 
-        for(int i = 0; i < nums1.size(); i++){
-            nums1[i] = pp[nums1[i]];
-        }
+        for(int i=nums2.size()-1; i>=0; i--){
+            mpp[nums2[i]] = -1;
 
-        return nums1;
+            while(!mono.empty() && mono.top()<=nums2[i]) mono.pop();
+            if(!mono.empty()) mpp[nums2[i]]=mono.top();
+            mono.push(nums2[i]);
+        }
+        vector<int> ans;
+        for(auto x : nums1){
+            ans.push_back(mpp[x]);
+        }
+        return ans;
     }
 };
